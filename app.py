71521c3e33564b0d4c70b034eefd309c2a96f960
Ali Nisar Haider <alinisarhaider@gmail.com>
2020-06-27 19:51:43 +0500

    official 2.0

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
 @app.route('/detect', methods=['POST'])
 def detect():
     form_values = [x for x in request.form.values()]
-    # url, keywords = form_values[0], form_values[1].split(',')#.split(' ')
+    url, keywords = form_values[0], form_values[1].split(',')#.split(' ')
     # audio_segment_buffer = get_audio_stream(video_url=url)
     # response, time_offset = transcribe_audio(audio_data=audio_segment_buffer, language='en-US')
     # transcription, timestamps_list = get_transcriptions(response_list=response, time_offset_list=time_offset)
